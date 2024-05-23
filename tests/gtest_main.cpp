2ae555eb7717c01da932183008325bc40e21d12f
luckydipper <qhrqufdlek@khu.ac.kr>
2024-05-22 21:22:45 -0400

    test: tutorial code

--- tests/gtest_main.cpp
+++ tests/gtest_main.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <hello.cc>
 
 int main(int argc, char **argv){
     ::testing::InitGoogleTest(&argc, &argv);
--- tests/hello_test.cc
+++ tests/hello_test.cc
@@ -1,6 +1,8 @@
-TEST(HelloTest, BasicAssertions) {
+#include <gtest/gtest.h>
+
+TEST(StringTest, BasicAssertions) {
   // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
+  EXPECT_STRNE("world", "world");
   // Expect equality.
   EXPECT_EQ(7 * 6, 42);
 }
