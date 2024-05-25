605baaeb5aa5446490b38122b9586223486f364c
luckydipper <qhrqufdlek@khu.ac.kr>
2024-05-24 22:00:58 -0400

    refector : Split mnist_parser function test, train
    TODO: test mnist image to contain 0~255.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
+#include "parser/mnists_parser.hpp"
 #include <iostream>
-
-#include "mnists_parser.hpp"
 using namespace std;
 
 int main() {
@@ -12,13 +11,17 @@ int main() {
   const string train_y_path = "../dataset/train/train-labels.idx1-ubyte";
   const string test_X_path = "../dataset/test/t10k-images.idx3-ubyte";
   const string test_y_path = "../dataset/test/t10k-labels.idx1-ubyte";
-  vector<vector<unsigned char> > train_X = getData(train_X_path);
+  vector<vector<unsigned char>> train_X = getInputImgs(train_X_path);
+  vector<int> train_y = getLabels(train_y_path);
+  vector<vector<unsigned char>> test_X = getInputImgs(test_X_path);
+  vector<int> test_y = getLabels(test_y_path);
 
-  for (const vector<unsigned char> &image : train_X) {
-    for (int i = 0; i < 28 * 28; i++) {
-      if ((i % 28) == 0) cout << "\n";
-      cout << image[i] << " ";
-    }
-  }
-  
+  // for (const vector<unsigned char> &image : train_X) {
+  //   for (int i = 0; i < 28 * 28; i++) {
+  //     if ((i % 28) == 0) cout << "\n";
+  //     cout << image[i] << " ";
+  //   }
+  // }
+  cout << train_X.size() << ", " << train_y.size() << "\n";
+  cout << test_X.size() << ", " << test_y.size() << "\n";
 }
--- src/mnists_parser.cpp
+++ src/mnists_parser.cpp
@@ -1,26 +1,49 @@
-#include "mnists_parser.hpp"
+#include "parser/mnists_parser.hpp"
 
-vector< vector<unsigned char> > getData(string path){
+vector< vector<unsigned char> > getInputImgs(const std::string& path){
     std::ifstream file(path, std::ios::binary);
     std::vector<std::vector<unsigned char> > images;
+    if (!file.is_open())
+        throw std::runtime_error("Could not open file: " + path);
+    
+    // ubyte 파일 포맷에 따라 첫 16바이트는 헤더 정보를 포함
+    file.seekg(16);
 
-    if (file.is_open()) {
-        // ubyte 파일 포맷에 따라 첫 16바이트는 헤더 정보를 포함
-        file.seekg(16);
+    while (true) {
+        std::vector<unsigned char> image(28 * 28);
+        if (!file.read(reinterpret_cast<char*>(image.data()), 28 * 28))
+            break; // 파일의 끝에 도달하면 읽기 중단
+        images.push_back(image);
+    }
 
-        while (true) {
-            std::vector<unsigned char> image(28 * 28);
-            if (!file.read(reinterpret_cast<char*>(image.data()), 28 * 28)) {
-                break; // 파일의 끝에 도달하면 읽기 중단
-            }
-
-            images.push_back(image);
-        }
-
-        file.close();
-    } else {
-        cout << "Unable to open file " << path << "\n";
-    }   //cerr
-
+    file.close();
     return images;
 }
+
+
+std::vector<int> getLabels(const std::string& path) {
+    // 파일을 바이너리 모드로 열기
+    std::ifstream file(path, std::ios::binary);
+    if (!file.is_open()) {
+        throw std::runtime_error("Could not open file: " + path);
+    }
+
+    // 파일 헤더를 읽어서 레이블의 수를 확인
+    int magic_number = 0;
+    int num_labels = 0;
+    file.read(reinterpret_cast<char*>(&magic_number), 4);
+    magic_number = __builtin_bswap32(magic_number); // 바이너리 데이터의 엔디안을 변경
+    file.read(reinterpret_cast<char*>(&num_labels), 4);
+    num_labels = __builtin_bswap32(num_labels); // 바이너리 데이터의 엔디안을 변경
+
+    // 레이블 데이터를 읽어서 벡터에 저장
+    std::vector<int> labels(num_labels);
+    for (int i = 0; i < num_labels; ++i) {
+        unsigned char label = 0;
+        file.read(reinterpret_cast<char*>(&label), 1);
+        labels[i] = static_cast<int>(label);
+    }
+
+    return labels;
+}
+
