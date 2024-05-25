7bf1b2403ac57d71b986d9402b013a583869aa0e
luckydipper <qhrqufdlek@khu.ac.kr>
2024-05-24 21:57:48 -0400

    rename: cc -> cpp

--- tests/hello_test.cpp
+++ tests/hello_test.cpp
@@ -2,7 +2,7 @@
 
 TEST(StringTest, BasicAssertions) {
   // Expect two strings not to be equal.
-  EXPECT_STRNE("world", "world");
+  EXPECT_STRNE("world", "m");
   // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+  EXPECT_EQ(7 * 6, 43);
 }
