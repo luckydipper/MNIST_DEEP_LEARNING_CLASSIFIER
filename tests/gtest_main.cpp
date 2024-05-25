c99ad4dff0abc39a2625f7950e4b8b8883140123
luckydipper <qhrqufdlek@khu.ac.kr>
2024-05-24 21:58:57 -0400

    test: mnist_parser

--- tests/gtest_main.cpp
+++ tests/gtest_main.cpp
@@ -1,7 +1,6 @@
 #include <gtest/gtest.h>
-#include <hello.cc>
 
 int main(int argc, char **argv){
-    ::testing::InitGoogleTest(&argc, &argv);
+    ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
---
+++ tests/test_parser.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+#include "parser/mnists_parser.hpp"
+
+TEST(mnists_parser, size_test){
+  const string train_X_path = CMAKE_SOURCE_DIR "/dataset/train/train-images.idx3-ubyte";
+  const string train_y_path = CMAKE_SOURCE_DIR "/dataset/train/train-labels.idx1-ubyte";
+  const string test_X_path = CMAKE_SOURCE_DIR "/dataset/test/t10k-images.idx3-ubyte";
+  const string test_y_path = CMAKE_SOURCE_DIR "/dataset/test/t10k-labels.idx1-ubyte";
+  vector<vector<unsigned char> > train_X = getInputImgs(train_X_path);
+  vector<int> train_y = getLabels(train_y_path);
+  vector<vector<unsigned char> > test_X = getInputImgs(test_X_path);
+  vector<int> test_y = getLabels(test_y_path);
+
+  EXPECT_EQ(train_X.size(), train_y.size());
+  EXPECT_EQ(test_X.size(), test_y.size());
+  
+}
+
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
